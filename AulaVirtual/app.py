f0a2cd789a1376a4a7ec38304084e108bba791ed
lucas <d47351988@alumnos.itr.edu.ar>
2024-11-08 00:20:44 -0300

    no me anda el html

--- AulaVirtual/app.py
+++ AulaVirtual/app.py
@@ -1,27 +1,11 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Configuración de la base de datos
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456@localhost/aula_virtual_db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-# Ejemplo de un modelo
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-
 @app.route('/')
 def index():
-    return "conectao a la base de dato"
+    materias = ['Matemáticas', 'Historia', 'Biología']  # Ejemplo de datos
+    return render_template('index.html', title="Aula Virtual", materias=materias)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
-
-
-import pymysql
-pymysql.install_as_MySQLdb()
+    app.run(debug=True)
