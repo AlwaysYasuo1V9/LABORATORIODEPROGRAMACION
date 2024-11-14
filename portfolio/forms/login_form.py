7a3724691a25d740c1def474f4ce99d71ef3cfbc
Lucas Benjamin Ramos <d47351988@instituto.itr.edu.ar>
2024-11-14 10:44:04 -0300

    portafoliando ando

---
+++ portfolio/forms/login_form.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class LoginForm(FlaskForm):
+    username = StringField('Usuario', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Contraseña', validators=[DataRequired()])
+    submit = SubmitField('Iniciar sesión')
